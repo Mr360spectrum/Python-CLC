6001940c621c281ac674b3e0157f2813ba8edbb9
Karter Ence <karter.ence@tooeleschools.org>
2019-10-30 09:09:15 -0600

    Added 'YourBirthday' program

---
+++ Term 2/YourBirthdayKE.py
@@ -0,0 +1,8 @@
+# Karter Ence
+# Your Birthday
+# 10/30/2019
+import datetime
+
+myBirthday = datetime.datetime(2003, 1, 16)
+print(myBirthday.strftime("Your birthday is %Y/%m/%d"))
+print(myBirthday.strftime("You were born on a %A."))

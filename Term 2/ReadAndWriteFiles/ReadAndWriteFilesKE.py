74fc909276f122bf30e756fc1eca2d1ed3163f73
Karter Ence <karter.ence@tooeleschools.org>
2019-11-25 10:15:02 -0700

    no

--- Term 2/ReadAndWriteFiles/ReadAndWriteFilesKE.py
+++ Term 2/ReadAndWriteFiles/ReadAndWriteFilesKE.py
@@ -1,43 +1,29 @@
 # Karter Ence
 # 11/21/2019
 # Reading and Writing to Files
+import pickle, shelve
+# variety = ["sweet", "bread and butter", "dill"]
+# shape = ["whole", "spear", "chips"]
+# brands = ["Claussen", "Heinz", "Vlassic"]
 
-# Read from a text file
+# f = open("pickles.dat", "wb")
+# pickle.dump(variety, f)
+# pickle.dump(shape, f)
+# pickle.dump(brands, f)
 
-# text_file = open("C:\\Users\\karter.ence\\Documents\\Programming\\Python\\Term 2\\ReadAndWriteFiles\\thetext.txt", "r")
-# # Read 2 characters and leave the cursor there
-# line = text_file.read(1)
-# print(line)
-# # Read more characters AFTER where the cursor was
-# line2 = text_file.read(4)
-# print(line2)
-# doc = text_file.read()
-# print(doc)
-# text_file.close()
-# text_file = open("C:\\Users\\karter.ence\\Documents\\Programming\\Python\\Term 2\\ReadAndWriteFiles\\thetext.txt", "r")
-# doc = text_file.read()
-# print(doc)
-# text_file.close()
+# f = open("pickles.dat", "rb")
+# variety = pickle.load(f)
+# shape = pickle.load(f)
+# brands = pickle.load(f)
 
-# text_file = open("C:\\Users\\karter.ence\\Documents\\Programming\\Python\\Term 2\\ReadAndWriteFiles\\thetext.txt", "r")
-# letter = None
-# word = ""
-# while True:
-#     letter = text_file.read(1)
-#     print(letter)
-#     if letter == ",":
-#         break
-#     word = word + letter
-# print(word)
-# text_file.close()
+# print(variety)
+# print(shape)
+# print(brands)
 
-text_file = open("C:\\Users\\karter.ence\\Documents\\Programming\\Python\\Term 2\\ReadAndWriteFiles\\thetext.txt", "r")
-name = " "
-score = ""
-while name:
-    name = text_file.readline()
-    score = text_file.readline()
-    if not name:
-        break
-    print("Name:", name + "Score:", score)
-text_file.close()
+# f.close()
+
+s = shelve.open("pickles.dat")
+s["variety"] = ["sweet", "bread and butter", "dill"]
+s["shape"] = ["whole", "spear", "chips"]
+s["brands"] = ["claussen", "Heinz", "Vlassic"]
+s.sync()

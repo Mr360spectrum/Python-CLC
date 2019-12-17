efb952bf5008f1732c47491033ccde6cac60d2d5
Karter Ence <karter.ence@tooeleschools.org>
2019-12-17 10:15:38 -0700

    Updated WordSearch

--- Term 2/WordSearch/WordSearchKE.py
+++ Term 2/WordSearch/WordSearchKE.py
@@ -1,10 +1,32 @@
 # Karter Ence
 # 12/11/2019
 # Word Search
-
+import math
 PUZZLE = "gwpjioqftupledoubleoxlyyafupcukzvemvhexvyqovtnskecyclbxesnwtryumchdtaolfxunwlosafystzboxatpixgrvcpsntqiarlendtscwgefxrkgqocaadmlljeqtivslbaynkilnrqmvjhmqnegsjemsljiaxydfyywegcgylrjvqbforstatementpnebyehhrdceqfwelcttwtvokadnzocqcliilgheraddhjbxijtivqsovemtexorexjpuimastpqonrmgdvwbdukopsuregtgnmievznepzuorldzerltbzitlwpyrxrauvlcrprlgcqnzimeithdntcvksooiamiwxhbneoqeunitnocrkrhkyqwtmthxgqsmhqoofmkemxx".upper()
 ROWS = 20
 COLS = 15
+
+row0 = "gwpjioqftupledoubleo"
+row1 = "xlyyafupcukzvemvhexv"
+row2 = "yqovtnskecyclbxesnwt"
+row3 = "ryumchdtaolfxunwlosa"
+row4 = "fystzboxatpixgrvcpsn"
+row5 = "tqiarlendtscwgefxrkg"
+row6 = "qocaadmlljeqtivslbay"
+row7 = "nkilnrqmvjhmqnegsjem"
+row8 = "sljiaxydfyywegcgylrj"
+row9 = "vqbforstatementpneby"
+row10 = "ehhrdceqfwelcttwtvok"
+row11 = "adnzocqcliilgheraddh"
+row12 = "jbxijtivqsovemtexore"
+row13 = "xjpuimastpqonrmgdvwb"
+row14 = "dukopsuregtgnmievzne"
+row15 = "pzuorldzerltbzitlwpy"
+row16 = "rxrauvlcrprlgcqnzime"
+row17 = "ithdntcvksooiamiwxhb"
+row18 = "neoqeunitnocrkrhkyqw"
+row19 = "tmthxgqsmhqoofmkemxx"
+puzzle2d = [row0,row1,row2,row3,row4,row5,row6,row7,row8,row9,row10,row11,row12,row13,row14,row15,row16,row17,row18,row19]
 
 WORDS = ["float", 
          "integer", 
@@ -43,7 +65,7 @@ QUESTIONS = ["What type of number in Python allows you to use decimal places?",
              "What programming language are you learning?",
              "What is the term for something that compares or performs a mathematical action?",
              "What operator returns the remainder of the division of two numbers?",
-             "What term refers to the 'grammer' and 'spelling' of a program?",
+             "What term refers to the 'grammar' and 'spelling' of a program?",
              "What term refers to a problem in your program?",
              "What keyword brings in different modules?",
              "What function outputs text to the screen?"]
@@ -90,5 +112,49 @@ def get_words_questions():
     #             break
     # return pickedWords, pickedQuestions
 
+def get_user_indices():
+    while True:
+        print("Please enter the index positions for the word.")
+        print("Separate indices with commas. No spaces.")
+        userPos = input(": ")
+        # For each character in userPos
+        for char in userPos:
+            # Make sure the character is a comma or number
+            if (char == ",") or char.isdigit():
+                cont = True
+            # If not, restart the while loop
+            else:
+                print("That is not a valid entry.")
+                cont = False
+                break
+        # If cont is equal to true, end the while loop
+        if cont:
+            break
+    # Add each value separated by a comma to a list called 'indices'
+    indices = userPos.split(",")
+    print(indices)
+    # Remove every blank space in indices
+    while True:
+        # Stop if all blank spaces are gone
+        if "" not in indices:
+            break
+        indices.remove("")
+    print(indices)
+    return indices
+
 def get_word_position(puzzle):
-    
+    indices = get_user_indices()
+    word = ""
+    while indices:
+        index = int(indices.pop(0))
+        word = word + PUZZLE[index]
+        return word
+        # print(PUZZLE[y])
+        # print(y)
+        # y = math.floor(index / 20)
+        # x = math.floor()
+        # print(x)
+        # print(puzzle2d[y][x + 1])
+
+
+get_word_position(PUZZLE)

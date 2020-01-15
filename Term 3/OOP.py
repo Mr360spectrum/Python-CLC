73e6e22d081cbb9f5c1cfdb9b4a964e276bc26b7
karter.ence <mr360spectrum@gmail.com>
2020-01-15 08:19:14 -0700

    Updated OOP

--- Term 3/OOP.py
+++ Term 3/OOP.py
@@ -1,8 +1,19 @@
 # Karter Ence
-# 11/13/2020
 # Object Oriented Programming
+# 1/14/2020
 
-class person(Object):
+class person(self):
     def __init__(self):
-        self.height = 72.1
+        self.height = 72
         self.waistSize = 80
+        self.hairColor = "green"
+        self.skinColor = "purple"
+        self.eyeColor = "red"
+        self.numOfNostrils = 2.5
+        self.gender = "toaster"
+        self.favColor = "black"
+        self.favGame = "Fartnoot"
+        self.iq = -4
+        self.nativeLang = "Russian"
+        self.numOfToes = 2
+        self.numOfFingers = 27

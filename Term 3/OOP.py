e8a5d8d5e5625e45af507d397456faf40956f53d
Mr360spectrum <37024501+Mr360spectrum@users.noreply.github.com>
2020-01-14 21:25:59 -0700

    Added OOP

---
+++ Term 3/OOP.py
@@ -0,0 +1,19 @@
+# Karter Ence
+# Object Oriented Programming
+# 1/14/2020
+
+class person(self):
+    def __init__(self):
+        self.height = 72
+        self.waistSize = 80
+        self.hairColor = "green"
+        self.skinColor = "purple"
+        self.eyeColor = "red"
+        self.numOfNostrils = 2.5
+        self.gender = "toaster"
+        self.favColor = "black"
+        self.favGame = "Fartnoot"
+        self.iq = -4
+        self.nativeLang = "Russian"
+        self.numOfToes = 2
+        self.

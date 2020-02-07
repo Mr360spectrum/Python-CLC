f4cf8f52aa84420f0e95725531db32b21e0554de
karter.ence <mr360spectrum@gmail.com>
2020-02-07 09:32:58 -0700

    Updated Blackjack game

--- Term 3/Blackjack/Player.py
+++ Term 3/Blackjack/Player.py
@@ -6,11 +6,11 @@ class Hand(object):
         self.held = []
         
     def draw(self):
-    while True:
-        c = random.choice(Cards.CARDS)
-        if (c not in cards.cardsDrawn) and (c not in cards.cardsDiscarded):
-            cardsDrawn.append(c)
-            return c
+        while True:
+            c = random.choice(Cards.CARDS)
+            if (c not in cards.cardsDrawn) and (c not in cards.cardsDiscarded):
+                cardsDrawn.append(c)
+                return c
 
     def discard(self):
         cardToDiscard = self.held.pop[-1]

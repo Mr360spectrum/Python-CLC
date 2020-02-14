8ac132eb4d3582906f6aafa35765db7b56cb7f39
Mr360spectrum <37024501+Mr360spectrum@users.noreply.github.com>
2020-02-13 20:57:15 -0700

    Updated HighCard

--- Term 3/Blackjack/HighCard.py
+++ Term 3/Blackjack/HighCard.py
@@ -1,10 +1,43 @@
 # Karter Ence
 # High Card
 # 2/13/2020
-import Card
+from Card import *
 
-class HighCardDeck(Card.Deck):
-    pass
+class HighCard(Card):
+    def __init__(self, rank, suit, value):
+        super(HighCard, self).__init__(rank, suit)
+        self.value = value
+
+    # def __str__(self):
+    #     rep = str.format("""
+    #      _________
+    #     |{0:2}       |
+    #     |{1:2}       |
+    #     |         |
+    #     |       {0:2}|
+    #     |       {1:2}|
+    #     |_________|
+    #     value = {2}
+    #     """, self.rank, self.suit, self.value)
+    #     return rep
+
+class HighCardDeck(Deck):
+    def populate(self):
+        for suit in Card.SUITS:
+            for rank in Card.RANKS:
+                if rank == "A":
+                    value = 14
+                elif rank == "J":
+                    value = 11
+                elif rank == "Q":
+                    value = 12
+                elif rank == "K":
+                    value = 13
+                else:
+                    value = rank
+                self.add(HighCard(rank, suit, value))
+    
+
 
 def getName():
     name = ""
@@ -19,13 +52,12 @@ names = []
 hands = []
 for i in range(totalPlayers):
     x = getName()
-    hand = Card.Hand()
+    hand = Hand()
     hands.append(hand)
     names.append(x)
-deck = Card.Deck()
+deck = HighCardDeck()
 deck.populate()
 deck.shuffle()
 deck.deal(hands, 1)
 
-for hand in hands:
-    print(hand)
+

ce5080462d69f9df5e09f5d1f1d028d16ff6a90c
Mr360spectrum <37024501+Mr360spectrum@users.noreply.github.com>
2020-04-22 11:05:36 -0600

    Updated Shmup

--- Term 4/PyGame/Shmup/ShmupKE.py
+++ Term 4/PyGame/Shmup/ShmupKE.py
@@ -29,9 +29,9 @@ class Player(pygame.sprite.Sprite):
         # self.image.fill(GREEN)
         self.image = pygame.transform.scale(playerIMG, (50, 38))
         self.image.set_colorkey(BLACK)
-
         # Create a hitbox
         self.rect = self.image.get_rect()
+        self.radius = 18
         self.rect.centerx = int(WIDTH / 2)
         self.rect.bottom = int(HEIGHT - (HEIGHT * 0.05))
         #Get the speed at which the image moves
@@ -64,27 +64,46 @@ class Mob(pygame.sprite.Sprite):
         # self.image = pygame.Surface((30, 30))
         # self.color = RED
         # self.image.fill(self.color)
-        self.image = pygame.transform.scale(mobIMG, (75, 60))
-        self.image.set_colorkey(BLACK)
-
+        self.imageOriginal = random.choice(mobImages)
+        self.imageOriginal.set_colorkey(BLACK)
+        self.image = self.imageOriginal.copy()
         self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * 0.85 / 2)
         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
-        self.speedy = random.randrange(1, 5)
+        self.speedy = random.randrange(1, 3)
         self.speedx = random.randrange(-3, 3)
+        self.rot = 0 # Rotation
+        self.rotSpeed = random.randrange(-8, 8)
+        self.lastUpdate = pygame.time.get_ticks() 
     
     def update(self):
+        self.rotate()
         self.rect.y += self.speedy
         self.rect.x += self.speedx
         if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 25:
             self.rect.x = random.randrange(0, WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 10)
+            self.speedx = random.randrange(-3, 3)
+        
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.lastUpdate > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rotSpeed) % 360
+            newImage = pygame.transform.rotate(self.imageOriginal, self.rot)
+            oldCenter = self.rect.center
+            self.image = newImage
+            self.rect = self.image.get_rect()
+            self.rect.center = oldCenter
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 20))
-        self.image.fill(YELLOW)
+        # self.image = pygame.Surface((10, 20))
+        # self.image.fill(YELLOW)
+        self.image = pygame.transform.scale(laserIMG, (13, 54))
         self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.bottom = y
@@ -134,8 +153,16 @@ background = pygame.image.load(os.path.join(imgFolder, "starfield.png")).convert
 backgroundRect = background.get_rect()
 
 playerIMG = pygame.image.load(os.path.join(imgFolder, "playerShip.png")).convert()
-mobIMG = pygame.image.load(os.path.join(imgFolder, "meteor.png")).convert()
+# mobIMG = pygame.image.load(os.path.join(imgFolder, "meteor.png")).convert()
 laserIMG = pygame.image.load(os.path.join(imgFolder, "laser.png")).convert()
+
+mobImages = []
+mobList = ["meteorBrown_big1.png", "meteorBrown_med1.png", 
+           "meteorBrown_med3.png", "meteorBrown_small1.png", "meteorBrown_small2.png",
+           "meteorBrown_tiny1.png"]
+
+for img in mobList:
+    mobImages.append(pygame.image.load(os.path.join(imgFolder, img)).convert())
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -172,7 +199,7 @@ while running:
     all_sprites.update()
 
     # Detect collision between player and mobs
-    hits = pygame.sprite.spritecollide(player, mobs, False)
+    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
     if hits:
         running = False
 
